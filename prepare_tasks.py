e1189b70ccd31675f970e5d5655ee8bff263f08c
Dmitriy Lyuosev <xhaustful@gmail.com>
2024-12-28 19:18:35 +0300

    fix: Task-Request relationship in prepare_tasks

--- prepare_tasks.py
+++ prepare_tasks.py
@@ -32,17 +32,34 @@ def get_urls(url_id):
         print(f"Something went wrong: {err}")
     return []
 
+def get_requests():
+    try:
+        response = requests.get('http://localhost:8000/requests', timeout=10)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.HTTPError as errh:
+        print(f"HTTP Error occurred: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error occurred: {errt}")
+    except requests.exceptions.RequestException as err:
+        print(f"Something went wrong: {err}")
+    return []
+
 def main():
     tasks = get_tasks()
-    null_request_tasks = [task for task in tasks if task['request_id'] is None]
     
     results_to_save = []
-    for task in null_request_tasks:
+    for task in tasks:
         urls = get_urls(task['url_id'])
+        requests_data = get_requests()
+        request_found = next((req for req in requests_data if req['task_id'] == task['id']), None)
         for url in urls:
             result = {
                 "task_id": task['id'],
                 "url": url['value'],
+                # "request_id": request_found['id'] if request_found else None,
             }
             results_to_save.append(result)
     
